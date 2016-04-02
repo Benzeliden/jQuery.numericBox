a57f94a1f7fb0418dd0b2eedde3346fb21f0bf3f
Benzeliden <denis.krasakov@gmail.com>
2016-04-02 20:14:56 +0300

    Options reworked. Add destroy function. Minified version added

--- jquery.numericBox.js
+++ jquery.numericBox.js
@@ -3,23 +3,19 @@
  *
  */
 (function($) {	
+	"use strict"
 	var notNumberRegexp = new RegExp('[^0-9]', 'g');
 	var pointSymbol = ".";
-	
-	var defaultOptions = {
-		scale: 6, 
-		max: 99999.999,
-		min: -99999.999,
-		separator: ".",
-	};
+	var typeOfFunction = typeof function(){};
+	var typeOfStr = typeof '';
+	var pluginName = 'numericBox';
 		
-	function fixNumber2(number, options){
+	function fixNumber(number, options){
 		var separatorPos = number.indexOf(options.separator);
-		var hasPoint = options.canBeFloat && separatorPos > -1;
+		var hasPoint = options.scale > 0 && separatorPos > -1;
 		var isNegative = number[0] == "-";
 		var start = isNegative ? 1 : 0;		
             		
-		//console.log("start", number, hasPoint, separatorPos);
 		var	resultNumberStr = number.substring(start, hasPoint ? separatorPos : number.length).replace(notNumberRegexp, '');
 		if (hasPoint) {
 			var fraction = number.substring(separatorPos + 1).replace(notNumberRegexp, '');
@@ -27,7 +23,6 @@
 		}
 		
 		var resultNumberInt = isNegative ? -resultNumberStr : +resultNumberStr;
-		//console.log("result str = '" + resultNumberStr + "', int = " + resultNumberInt);
 		
 		var isOutOfRange = false;
 		if (resultNumberInt > options.max){
@@ -44,42 +39,133 @@
 			
 		return (isNegative ? "-" : "") + resultNumberStr.replace(pointSymbol, options.separator);
 	}
-		
-	//console.log(fixNumber2("333.4,5.6.",defaultOptions));
-	//console.log(fixNumber2("3334343433,4.5.6.",defaultOptions));
-	//console.log(fixNumber2("-33-553.4,5.6.",defaultOptions));
 	
+	function ProcessSetup(options, setupOptions, $elem) {
+		var $data = $elem.data();
 	
-    $.fn.numericBox = function( options ) {
-		
-        options = $.extend( defaultOptions, options );				
-		options.canBeFloat = options.scale > 0;
-		
-		var previousSelection = -1;
-		var previousValue = this.value;
-		function fixSelection(elem, selectionPosition){
+		for (var optionName in setupOptions) {
+			if (setupOptions.hasOwnProperty(optionName)) {
+				var optionConfig = setupOptions[optionName];
+				var configType = typeof optionConfig;
+				var optionVal = undefined;
+
+				switch(configType) {
+					case typeOfStr:
+						var attrs = optionConfig.split(',');
+						for	(var i = 0, len = attrs.length; i < len; i++){
+							if ($data[attrs[i]] != undefined){
+								optionVal = $data[attrs[i]];	
+								break;								
+							}
+						}
+						break;
+					case typeOfFunction:
+						optionVal = optionConfig($elem);
+						break;
+				}
+
+				if (optionVal !== undefined) {
+					options[optionName] = optionVal;
+				}
+			}
+		}
+	}
+	
+	function numericBox(element, options) {
+        var baseOptions = $.extend({}, $.fn.numericBox.defaultOptions),
+            $this = $(element),
+			self = this;
 			
+		var setup = $.extend({}, $.fn.numericBox.defaultOptions.setup, options != undefined ? options.setup : undefined);
+		
+		ProcessSetup(baseOptions, setup, $this);
+
+        self.options = $.extend(baseOptions, options);
+		self.destroy = function(){			
+			$this
+				.off('keydown keypress keyup paste input', processInput)
+				.off("blur", processBlur)
+				.off("focus", processFocus)
+				.removeData(pluginName, undefined);
+		};
+		self.init = function(){			
+			$this
+				.on('keydown keypress keyup paste input', processInput)
+				.on("blur", processBlur)
+				.on("focus", processFocus)
+				.data(pluginName,self);
+		}
+		self.setOptions = function(newOptions){
+			$.extend(self.options, newOptions);
+			processInput.call(element);
 		}
 		
-        $( this ).on('keydown keyup paste input', function() {
-			
-			var newValue = fixNumber2(this.value, options);
-			if (newValue !== this.value){
-				this.value = newValue;
-				
-				//if user enter invalid character return caret to previous position
-				if (previousSelection != -1 && newValue == previousValue) {
-					this.setSelectionRange(previousSelection,previousSelection);
-				}				
-				previousValue = newValue;
-			}
-			previousSelection = this.selectionStart;
+		
+        var previousSelection = -1;
+        var previousValidValue = element.value;
+		function processInput() {
+			var input = this;
+			var oldValue = input.value;
+            var newValue = fixNumber(oldValue, self.options);
+            if (newValue !== oldValue) {
+                input.value = newValue;
+                //if user enter invalid character return caret to previous position
+                if (previousSelection !== -1 && newValue === previousValidValue) {
+                    input.setSelectionRange(previousSelection, previousSelection);
+                }
+            }
+			previousValidValue = newValue;
+            previousSelection = input.selectionStart;
             return true;
-        }).on("blur", function() {
-			if (this.value.length == 0 || this.value ==  "-") {
-				this.value = "0";
+        }
+		function processBlur() {
+			var input = this;
+            if (input.value.length === 0 || input.value === "-") {
+                input.value = "0";
+            }
+            previousSelection = -1;
+        }
+		function processFocus(){
+			previousSelection = this.selectionStart;	
+		}
+
+    }
+	
+    $.fn.numericBox = function( options , parameter) {			
+        for (var i = 0, len = this.length; i < len; i++) {
+			if (undefined == $(this[i]).data(pluginName)) {
+				var plugin = new numericBox(this[i], options);
+				plugin.init();
+			} else{
+				var plugin = $(this[i]).data(pluginName);
+				var optionType = typeof options;
+				if (optionType == typeOfStr){
+					if (typeof plugin[options] == typeOfFunction){
+						plugin[options](parameter);
+					}
+				}
 			}
-		});
-        return $( this );
+        }
+
+        return $(this);
     };
+	
+	$.fn.numericBox.defaultOptions = {
+		scale: 2,
+		separator: '.',
+		min: 0,
+		max: 100,
+		//string or function can be used
+		//string represent html5 data-attribute names separated by comma
+		//function should have signature function($input) 
+		//where $input - jQuery object of current input
+		//function should return valid value - no validation check here!!		
+		setup: {
+			min:'nboxMin,valRangeMin',
+			max:'nboxMax,valRangeMax',
+			scale:'nboxScale',
+			separator:'nboxSeparator',
+		}
+	};
+	
 })( jQuery );
--- readme.md
+++ readme.md
@@ -5,13 +5,47 @@ Simple usage:
 $('input.numeric-input').numericBox();
 
 Options:
-  scale - maximum number of decimal places. Note: if scale set to 0 textbox accept only integer values.
-  max - max value for textbox
-  min - min value for textbox
+  scale - maximum number of decimal places. Note: if scale set to 0 input accept only integer values.
+  min/max - min/max value for input
   separator - decimal places separator
+  setup - configurable setup for all options.
+  
+Default options can be found in $.fn.numericBox.defaultOptions object
+
+Examples:
+
+1) Configure global options
+
+//remove min/max correction
+$.fn.numericBox.defaultOptions.max = Infinity; 
+$.fn.numericBox.defaultOptions.min = -Infinity; 
+
+2) Setup options from html5 attributes
+
+Html:
+
+<input class='numeric' data-my-min-value='-40' data-another-max='60' />
+
+JS:
+
+var options = {
+	setup : { 
+		min : 'myMinValue', 
+		max: 'anotherMax'
+	}
+}
+$('.numeric').numericBox(options);
+
+3) Remove plugin:
+
+$('#target').numericBox('destroy');
+
+4) Update options:
+
+$('#target').numericBox('setOptions', {scale:4});
+
+
 
 todo:
- refactor options
- allow not entering min & max
- not allow minus sign if option.min >= 0
  add version and manifest for jQuery plugin
+ format readme
